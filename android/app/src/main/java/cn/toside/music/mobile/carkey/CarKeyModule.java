0e41005dd2e9170da2b374071677c34102a2dbaf
carlito12345 <carlito12345@users.noreply.github.com>
2026-07-24 09:04:41 -0400

    fix: 添加吉利/亿咖通车机专用方向盘键值(200XXX系列)

--- android/app/src/main/java/cn/toside/music/mobile/carkey/CarKeyModule.java
+++ android/app/src/main/java/cn/toside/music/mobile/carkey/CarKeyModule.java
@@ -75,11 +75,35 @@ public class CarKeyModule extends ReactContextBaseJavaModule {
     WritableMap params = Arguments.createMap();
     params.putInt("keyCode", keyCode);
     
-    // Map some additional key codes used by different car brands
-    if (keyCode == 79 || keyCode == 126 || keyCode == 127) {
-      // KEYCODE_HEADSETHOOK(79), KEYCODE_MEDIA_PLAY(126), KEYCODE_MEDIA_PAUSE(127)
+    // Map Geely/ECarX custom steering wheel key codes
+    if (keyCode == 200087) { // KEYCODE_R_MEDIA_NEXT
+      keyCode = android.view.KeyEvent.KEYCODE_MEDIA_NEXT;
+    } else if (keyCode == 200088) { // KEYCODE_R_MEDIA_PREVIOUS
+      keyCode = android.view.KeyEvent.KEYCODE_MEDIA_PREVIOUS;
+    } else if (keyCode == 200085) { // KEYCODE_R_MEDIA_PLAY_PAUSE
       keyCode = android.view.KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE;
-    } else if (keyCode == 90 || keyCode == 127) {
+    } else if (keyCode == 200082) { // KEYCODE_R_MENU
+      keyCode = android.view.KeyEvent.KEYCODE_MENU;
+    } else if (keyCode == 200003) { // KEYCODE_R_HOME
+      keyCode = android.view.KeyEvent.KEYCODE_HOME;
+    } else if (keyCode == 200024) { // KEYCODE_R_VOLUME_UP
+      keyCode = android.view.KeyEvent.KEYCODE_VOLUME_UP;
+    } else if (keyCode == 200025) { // KEYCODE_R_VOLUME_DOWN
+      keyCode = android.view.KeyEvent.KEYCODE_VOLUME_DOWN;
+    } else if (keyCode == 200164) { // KEYCODE_R_VOLUME_MUTE
+      keyCode = android.view.KeyEvent.KEYCODE_VOLUME_MUTE;
+    } else if (keyCode == 200231) { // KEYCODE_R_VOICE_ASSIST
+      keyCode = android.view.KeyEvent.KEYCODE_SEARCH;
+    } else if (keyCode == 210001) { // KEYCODE_R_NAVI
+      keyCode = android.view.KeyEvent.KEYCODE_NAVIGATE_IN;
+    } else if (keyCode == 200600) { // KEYCODE_R_MULTI_FUNCTION
+      keyCode = android.view.KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE;
+    } else if (keyCode == 210005) { // KEYCODE_R_SEEK_NEXT
+      keyCode = android.view.KeyEvent.KEYCODE_MEDIA_FAST_FORWARD;
+    } else if (keyCode == 210006) { // KEYCODE_R_SEEK_REVIOUS
+      keyCode = android.view.KeyEvent.KEYCODE_MEDIA_REWIND;
+    } else if (keyCode == 79 || keyCode == 126 || keyCode == 127) {
+      // KEYCODE_HEADSETHOOK(79), KEYCODE_MEDIA_PLAY(126), KEYCODE_MEDIA_PAUSE(127)
       keyCode = android.view.KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE;
     }
     switch (keyCode) {
