78b49e7892a289700ce423bdeda2a2db4e1bd6c5
carlito12345 <carlito12345@users.noreply.github.com>
2026-07-26 12:14:00 -0400

    fix: 修复MiniPlayer未注册-AppRegistry.registerComponent添加

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
  * @format
  */
 import './shim'
+import { AppRegistry } from 'react-native'
+import MiniPlayer from './src/screens/MiniPlayer'
+
 import './src/app'
+
+AppRegistry.registerComponent('MiniPlayer', () => MiniPlayer)
 // import './test'
 // import '@/utils/errorHandle'
 // Navigation.registerComponent('com.myApp.WelcomeScreen', () => App)
